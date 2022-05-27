218d5dc352fe6a869bd4fa928e4eb32e54cea9e8
Adarsh <>
2022-05-27 22:49:56 +0530

    UI changes

--- Assets/External/Scripts/Launcher.cs
+++ Assets/External/Scripts/Launcher.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private TMP_InputField roomNameInputField;
+    [SerializeField] private TMP_Text errorMsg;
+    
     void Start()
     {
         Debug.Log($"Connecting");
@@ -22,5 +27,25 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuHandler.Instance.OpenMenu("titleMenu");
         Debug.Log($"OnJoinedLobby");
+    }
+
+    public void CreateRoom()
+    {
+        if(string.IsNullOrEmpty(roomNameInputField.text))
+            return;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuHandler.Instance.OpenMenu("loadingMenu");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuHandler.Instance.OpenMenu("roomMenu");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode,string msg)
+    {
+        errorMsg.text = $"Room creation failed {msg}";
+        MenuHandler.Instance.OpenMenu("errorMenu");
     }
 }
